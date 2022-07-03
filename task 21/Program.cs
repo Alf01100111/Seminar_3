c825b3d6a485f7b24f19509e1e3290949c1b0e25
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-03 21:46:58 +0300

    distance

---
+++ task 21/Program.cs
@@ -0,0 +1,39 @@
+﻿
+int[] CoordEnter(string dotname)
+{
+    System.Console.WriteLine($"Введите координаты точки {dotname} (X, Y, Z)");
+    int[] resultarr = new int[3];
+    for (int i = 0; i < resultarr.Length; i++)
+    {
+        resultarr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return resultarr;
+}
+
+// void PrintArr(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         System.Console.Write($" {arr[i]}");
+//     }
+// }
+
+int Distance(int[] dot1, int[] dot2)
+{
+    int[] deltas = new int[3];
+    for (int i = 0; i < 3; i++)
+    {
+        deltas[i] = dot1[i] - dot2[i];
+    }
+    double result = Math.Sqrt(Math.Pow(deltas[0], 2) + Math.Pow(deltas[1], 2) + Math.Pow(deltas[2], 2));
+    return result;
+}
+
+
+int[] dotcoordA = CoordEnter("A");
+int[] dotcoordB = CoordEnter("B");
+
+System.Console.WriteLine(Distance(dotcoordA,dotcoordB));
+
+PrintArr(dotcoordA);
+
