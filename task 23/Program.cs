6fd93164585a97f7272d4f664d2527ec75bbb7ac
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-03 23:03:18 +0300

    куб готов

---
+++ task 23/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("ВВедите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void CubeTable(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        int result = i * i * i;
+        Console.Write($"{result},");
+    }
+}
+
+CubeTable(number);
